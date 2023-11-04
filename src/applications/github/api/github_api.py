2e489ea63585617e885b3241b6aad2b6f7b6fd9b
Katarzyna Juszczak <katarzyna.juszczak@globallogic.com>
2023-11-04 19:14:38 +0100

    update api tests

--- src/applications/github/api/github_api.py
+++ src/applications/github/api/github_api.py
@@ -1,32 +1,45 @@
 import requests
+from src.logger.logger import AutomationFrameworkLogger
+from src.config.config import Config, JSONConfigProvider
+
+TestApiLogger = AutomationFrameworkLogger.get_logger("api_tests")
+config = Config(config_providers=[JSONConfigProvider(env_type="github_api")])
 
 
 class GitHubAPIClient:
-    """Current class contains every API call we use in tests"""
+    """Current class contains every API call we use in tests."""
 
     def __init__(self) -> None:
         pass
 
     def search_user(self, user_name):
-        print("Sending request to url: https://api.github.com/search/users")
-        r = requests.get("https://api.github.com/search/users", params={'q': user_name})
+        search_user_url = config.URLS["GITHUB_API_URL"] + config.URLS["SEARCH_USERS_URL"]
+        TestApiLogger.info(f"Sending request to url: {search_user_url}")
 
+        r = requests.get(f"{search_user_url}", params={'q': user_name})
         body = r.json()
-        print(f"Response retrieved {body}")
+
+        TestApiLogger.info(f"Response retrieved {body}")
+
         return body
 
     def search_repos(self, repo_name):
-        print("Sending request to url: https://api.github.com/search/repositories")
-        r = requests.get("https://api.github.com/search/repositories", params={'q': repo_name})
+        search_repos_url = config.URLS["GITHUB_API_URL"] + config.URLS["SEARCH_REPOS_URL"]
+        TestApiLogger.info(f"Sending request to url: {search_repos_url}")
 
+        r = requests.get(f"{search_repos_url}", params={'q': repo_name})
         body = r.json()
-        print(f"Response retrieved {body}")
+
+        TestApiLogger.info(f"Response retrieved {body}")
+
         return body
 
     def search_commits(self, commit_hash):
-        print("Sending request to url https://api.github.com/search/commits")
-        r = requests.get("https://api.github.com/search/commits", params={'q': commit_hash})
+        search_commits_url = config.URLS["GITHUB_API_URL"] + config.URLS["SEARCH_COMMITS_URL"]
+        TestApiLogger.info(f"Sending request to url: {search_commits_url}")
 
+        r = requests.get(f"{search_commits_url}", params={'q': commit_hash})
         body = r.json()
-        print(f"Response retrieved {body}")
+
+        TestApiLogger.info(f"Response retrieved {body}")
         return body
--- tests/github/api/test_api.py
+++ tests/github/api/test_api.py
@@ -1,38 +1,67 @@
-def test_search_for_existing_user(github_api_client):
-    existing_user_name = 'KatarzynaJuszczak'
-    users = github_api_client.search_user(existing_user_name)
-
-    print(f"Checking total count is {users['total_count']}")
-    assert users['total_count'] != 0
+from src.applications.github.api.github_api import TestApiLogger, config
 
 
-def test_search_for_nonexisting_user(github_api_client):
-    nonexisting_user_name = 'sjkfkjhsakjfsdajfsdjfjksdfjddjfdfjkdf'
-    users = github_api_client.search_user(nonexisting_user_name)
+def test_search_for_existing_user(github_api_app):
+    TestApiLogger.info("------- test_search_for_existing_user -------")
 
-    print(f"Checking total count is {users['total_count']}")
-    assert users['total_count'] == 0
+    existing_user_name = config.VALID_DATA["USER_NAME"]
+    users = github_api_app.search_user(existing_user_name)
 
+    TestApiLogger.info(f"Total count is {users['total_count']}")
 
-def test_search_for_existing_repo(github_api_client):
-    existing_repo_name = 'BecomeQAEngineerCourse'
-    repos = github_api_client.search_repos(existing_repo_name)
-
-    print(f"Checking total count is {repos['total_count']}")
-    assert repos['total_count'] != 0
+    assert users["total_count"] != 0
 
 
-def test_search_for_nonexisting_repo(github_api_client):
-    nonexisting_repo_name = 'sfbsfbhbjfjsfsdsdjfhsfjadfh'
-    repos = github_api_client.search_repos(nonexisting_repo_name)
+def test_search_for_non_existing_user(github_api_app):
+    TestApiLogger.info("------- test_search_for_non_existing_user -------")
 
-    print(f"Checking total count is {repos['total_count']}")
-    assert repos['total_count'] == 0
+    non_existing_user_name = config.NON_VALID_DATA["USER_NAME"]
+    users = github_api_app.search_user(non_existing_user_name)
+
+    TestApiLogger.info(f"Total count is {users['total_count']}")
+
+    assert users["total_count"] == 0
 
 
-def test_search_for_nonexisting_commit_hash(github_api_client):
-    nonexisting_commit_hash = 'lnasdklfnkjanskdjfnkjabsdjhfuyagsuygfuyagsduyfgauysdf'
-    commit_hashes = github_api_client.search_commits(nonexisting_commit_hash)
+def test_search_for_existing_repo(github_api_app):
+    TestApiLogger.info("------- test_search_for_existing_repo -------")
 
-    print(f"Checking total count is {commit_hashes['total_count']}")
-    assert commit_hashes['total_count'] == 0
+    existing_repo_name = config.VALID_DATA["REPO_NAME"]
+    repos = github_api_app.search_repos(existing_repo_name)
+
+    TestApiLogger.info(f"Total count is {repos['total_count']}")
+
+    assert repos["total_count"] != 0
+
+
+def test_search_for_non_existing_repo(github_api_app):
+    TestApiLogger.info("------- test_search_for_non_existing_repo -------")
+
+    non_existing_repo_name = config.NON_VALID_DATA["REPO_NAME"]
+    repos = github_api_app.search_repos(non_existing_repo_name)
+
+    TestApiLogger.info(f"Total count is {repos['total_count']}")
+
+    assert repos["total_count"] == 0
+
+
+def test_search_for_existing_commit_hash(github_api_app):
+    TestApiLogger.info("------- test_search_for_existing_commit_hash -------")
+
+    existing_commit_hash = config.VALID_DATA["COMMIT_HASH"]
+    commit_hashes = github_api_app.search_commits(existing_commit_hash)
+
+    TestApiLogger.info(f"Total count is {commit_hashes['total_count']}")
+
+    assert commit_hashes["total_count"] != 0
+
+
+def test_search_for_non_existing_commit_hash(github_api_app):
+    TestApiLogger.info("------- test_search_for_non_existing_commit_hash -------")
+
+    non_existing_commit_hash = config.NON_VALID_DATA["COMMIT_HASH"]
+    commit_hashes = github_api_app.search_commits(non_existing_commit_hash)
+
+    TestApiLogger.info(f"Total count is {commit_hashes['total_count']}")
+
+    assert commit_hashes["total_count"] == 0
