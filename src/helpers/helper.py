3153155dad28eed2eda45a616a2baff0dcd23c8b
Katarzyna Juszczak <katarzyna.juszczak@globallogic.com>
2023-11-10 20:28:26 +0100

    add create_rendom_data() method to helper

---
+++ src/helpers/helper.py
@@ -0,0 +1,14 @@
+import string
+import random
+
+
+def create_random_data(length=10, digits=True, letters=True):
+    characters = ""
+    if digits:
+        characters += string.digits
+    if letters:
+        characters += string.ascii_letters
+
+    random_data = "".join(random.choice(characters) for _ in range(length))
+
+    return random_data
